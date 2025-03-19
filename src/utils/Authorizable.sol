7b6964ffb2072accddba8e89d160d2f707d56dd5
Giles Chang <giles.chang@circle.com>
2025-03-19 21:50:24 +0800

    feat(CCS-1502): add util contracts (#2)
    This pull request introduces several new Solidity interfaces and utility
    contracts. The most important changes include the addition of interfaces
    as well as utility contracts for authorization and ownership management.
    Most of the utils and interfaces are forked from openzeppelin.
    
    New interfaces:
    
    Created the `ITransferWithWitness` interface for transfers with
    additional witness verification, including methods for managing
    witnesses and verifying transfer intents.
    
    New Utility contracts:
    
    * Introduced the `Authorizable` contract for managing authorized
    addresses, including methods for updating and validating authorizers.
    * Introduced the `Sweepable` contract for managing authorized sweeper
    addresses, inherited Authorizable, including methods for updating and
    validating sweeper.
    * Introduced the `Rescuable` contract for managing authorized rescuer
    addresses, inherited Authorizable, including methods for updating and
    validating rescuer. The contract provides the ERC20 rescue function for
    transfer the ERC20 token.

---
+++ src/utils/Authorizable.sol
@@ -0,0 +1,73 @@
+/**
+ * SPDX-License-Identifier: UNLICENSED
+ *
+ * Copyright (c) 2025, Circle Internet Financial Trading Company Limited.
+ * All rights reserved.
+ *
+ * Circle Internet Financial Trading Company Limited CONFIDENTIAL
+ *
+ * This file includes unpublished proprietary source code of Circle Internet
+ * Financial Trading Company Limited, Inc. The copyright notice above does not
+ * evidence any actual or intended publication of such source code. Disclosure
+ * of this source code or any related proprietary information is strictly
+ * prohibited without the express written permission of Circle Internet Financial
+ * Trading Company Limited.
+ */
+pragma solidity 0.8.24;
+
+import {Ownable} from "./Ownable.sol";
+
+/**
+ * @title Authorizable
+ */
+abstract contract Authorizable is Ownable {
+    mapping(bytes32 => address) private _authorized;
+
+    error InvalidAuthorizer(bytes32 role);
+    error NotAuthorizer(bytes32 role, address addr);
+
+    event AuthorizerChanged(address indexed addr, bytes32 indexed role);
+    event AuthorizerRemoved(bytes32 indexed role);
+
+    constructor() Ownable(msg.sender) {}
+
+    /**
+     * @notice Returns current authorizer
+     * @param role Role
+     * @return Authorizer's address
+     */
+    function _authorizer(bytes32 role) internal view returns (address) {
+        return _authorized[role];
+    }
+
+    /**
+     * @notice Authorize an address
+     * @param role Role
+     * @param addr Address
+     */
+    function _updateAuthorizer(bytes32 role, address addr) internal onlyOwner {
+        if (addr == address(0)) revert InvalidAuthorizer(role);
+        _authorized[role] = addr;
+
+        emit AuthorizerChanged(addr, role);
+    }
+
+    /**
+     * @notice Deauthorize an address
+     * @param role Role
+     */
+    function _removeAuthorizer(bytes32 role) internal onlyOwner {
+        delete _authorized[role];
+
+        emit AuthorizerRemoved(role);
+    }
+
+    /**
+     * @notice Validate if an address is authorized
+     * @param role Role
+     * @param addr Address
+     */
+    function _validAuthorizer(bytes32 role, address addr) internal view {
+        if (addr != _authorized[role]) revert NotAuthorizer(role, addr);
+    }
+}
---
+++ src/utils/Context.sol
@@ -0,0 +1,42 @@
+/**
+ * SPDX-License-Identifier: UNLICENSED
+ *
+ * Copyright (c) 2025, Circle Internet Financial Trading Company Limited.
+ * All rights reserved.
+ *
+ * Circle Internet Financial Trading Company Limited CONFIDENTIAL
+ *
+ * This file includes unpublished proprietary source code of Circle Internet
+ * Financial Trading Company Limited, Inc. The copyright notice above does not
+ * evidence any actual or intended publication of such source code. Disclosure
+ * of this source code or any related proprietary information is strictly
+ * prohibited without the express written permission of Circle Internet Financial
+ * Trading Company Limited.
+ */
+pragma solidity 0.8.24;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ *
+ * Forked from https://github.com/OpenZeppelin/openzeppelin-contracts/blob/6ba452dea4258afe77726293435f10baf2bed265/contracts/utils/Context.sol
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+
+    function _contextSuffixLength() internal view virtual returns (uint256) {
+        return 0;
+    }
+}
