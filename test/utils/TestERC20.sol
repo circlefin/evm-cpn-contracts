458c40bd32d8407a15b4cc0d3d41b0bc04421a01
Giles Chang <giles.chang@circle.com>
2025-03-24 10:01:03 +0800

    feat(CCS-1502): transfer with witness POC (#3)
    This pull request introduces significant changes to the
    `TransferWithWitness` contract and related test utilities. The primary
    focus is on implementing ERC20 transfer functionality with additional
    witness signatures and providing internal implementation for
    gas-abstracted transfers in the test utilities.
    
    ### New Contract Implementation:
    
    * **`TransferWithWitness` Contract**: Implements ERC20 transfer
    functionality with additional witness signatures, including methods for
    adding/removing witnesses, validating witness signatures, and handling
    transfer operations.
    
    ### Test Utilities:
    
    * **`TestEIP3009` Contract**: Provides internal implementation for
    gas-abstracted transfers, including methods for executing transfers with
    signed authorizations and validating signatures.
    
    These changes enhance the security and flexibility of ERC20 token
    transfers by incorporating witness signatures and gas-abstracted
    transfer mechanisms.

---
+++ test/utils/TestERC20.sol
@@ -0,0 +1,152 @@
+/**
+ * SPDX-License-Identifier: UNLICENSED
+ *
+ * Copyright (c) 2025, Circle Internet Financial Trading Company Limited.
+ * All rights reserved.
+ *
+ * Circle Internet Financial Trading Company Limited CONFIDENTIAL
+ *
+ * This file includes unpublished proprietary source code of Circle Internet
+ * Financial Trading Company Limited, Inc. The copyright notice above does not
+ * evidence any actual or intended publication of such source code. Disclosure
+ * of this source code or any related proprietary information is strictly
+ * prohibited without the express written permission of Circle Internet Financial
+ * Trading Company Limited.
+ */
+pragma solidity 0.8.24;
+
+import {TestEIP3009} from "./TestEIP3009.sol";
+
+// for the simplicity, TestERC721 is ERC721
+contract TestERC20 is TestEIP3009 {
+    constructor(string memory name_, string memory symbol_) TestEIP3009(name_, symbol_) {
+        _mint(msg.sender, 1000000 * 10 ** 18);
+    }
+
+    /**
+     * @notice Execute a transfer with a signed authorization
+     * @param from          Payer's address (Authorizer)
+     * @param to            Payee's address
+     * @param value         Amount to be transferred
+     * @param validAfter    The time after which this is valid (unix time)
+     * @param validBefore   The time before which this is valid (unix time)
+     * @param nonce         Unique nonce
+     * @param v             v of the signature
+     * @param r             r of the signature
+     * @param s             s of the signature
+     */
+    function transferWithAuthorization(
+        address from,
+        address to,
+        uint256 value,
+        uint256 validAfter,
+        uint256 validBefore,
+        bytes32 nonce,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        _transferWithAuthorization(from, to, value, validAfter, validBefore, nonce, v, r, s);
+    }
+
+    /**
+     * @notice Receive a transfer with a signed authorization from the payer
+     * @dev This has an additional check to ensure that the payee's address
+     * matches the caller of this function to prevent front-running attacks.
+     * @param from          Payer's address (Authorizer)
+     * @param to            Payee's address
+     * @param value         Amount to be transferred
+     * @param validAfter    The time after which this is valid (unix time)
+     * @param validBefore   The time before which this is valid (unix time)
+     * @param nonce         Unique nonce
+     * @param v             v of the signature
+     * @param r             r of the signature
+     * @param s             s of the signature
+     */
+    function receiveWithAuthorization(
+        address from,
+        address to,
+        uint256 value,
+        uint256 validAfter,
+        uint256 validBefore,
+        bytes32 nonce,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        _receiveWithAuthorization(from, to, value, validAfter, validBefore, nonce, v, r, s);
+    }
+
+    /**
+     * @notice Attempt to cancel an authorization
+     * @dev Works only if the authorization is not yet used.
+     * @param authorizer    Authorizer's address
+     * @param nonce         Nonce of the authorization
+     * @param v             v of the signature
+     * @param r             r of the signature
+     * @param s             s of the signature
+     */
+    function cancelAuthorization(address authorizer, bytes32 nonce, uint8 v, bytes32 r, bytes32 s) external {
+        _cancelAuthorization(authorizer, nonce, v, r, s);
+    }
+
+    /**
+     * @notice Execute a transfer with a signed authorization
+     * @dev EOA wallet signatures should be packed in the order of r, s, v.
+     * @param from          Payer's address (Authorizer)
+     * @param to            Payee's address
+     * @param value         Amount to be transferred
+     * @param validAfter    The time after which this is valid (unix time)
+     * @param validBefore   The time before which this is valid (unix time)
+     * @param nonce         Unique nonce
+     * @param signature     Signature bytes signed by an EOA wallet or a contract wallet
+     */
+    function transferWithAuthorization(
+        address from,
+        address to,
+        uint256 value,
+        uint256 validAfter,
+        uint256 validBefore,
+        bytes32 nonce,
+        bytes memory signature
+    ) external {
+        _transferWithAuthorization(from, to, value, validAfter, validBefore, nonce, signature);
+    }
+
+    /**
+     * @notice Receive a transfer with a signed authorization from the payer
+     * @dev This has an additional check to ensure that the payee's address
+     * matches the caller of this function to prevent front-running attacks.
+     * EOA wallet signatures should be packed in the order of r, s, v.
+     * @param from          Payer's address (Authorizer)
+     * @param to            Payee's address
+     * @param value         Amount to be transferred
+     * @param validAfter    The time after which this is valid (unix time)
+     * @param validBefore   The time before which this is valid (unix time)
+     * @param nonce         Unique nonce
+     * @param signature     Signature bytes signed by an EOA wallet or a contract wallet
+     */
+    function receiveWithAuthorization(
+        address from,
+        address to,
+        uint256 value,
+        uint256 validAfter,
+        uint256 validBefore,
+        bytes32 nonce,
+        bytes memory signature
+    ) external {
+        _receiveWithAuthorization(from, to, value, validAfter, validBefore, nonce, signature);
+    }
+
+    /**
+     * @notice Attempt to cancel an authorization
+     * @dev Works only if the authorization is not yet used.
+     * EOA wallet signatures should be packed in the order of r, s, v.
+     * @param authorizer    Authorizer's address
+     * @param nonce         Nonce of the authorization
+     * @param signature     Signature bytes signed by an EOA wallet or a contract wallet
+     */
+    function cancelAuthorization(address authorizer, bytes32 nonce, bytes memory signature) external {
+        _cancelAuthorization(authorizer, nonce, signature);
+    }
+}
